59a94e1527ccb7b0d9fd287d84c000e217ef4dec
Mahmoud Salah <ms685116@gmail.com>
2023-08-22 22:12:59 +0300

    push and pall

---
+++ push_pall.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * push - pushes an element to a stack
+ * @head: pointer to top element of the stack
+ * @n: element to be added
+*/
+void push(stack_t **head, int n)
+{
+	stack_t *node = malloc(sizeof(stack_t));
+
+	if (node == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	if (*head == NULL)
+	{
+		*head = node;
+	}
+	else
+	{
+		node->next = *head;
+		(*head)->prev = node;
+		*head = node;
+	}
+}
+/**
+ * pall - prints all the values on the stack
+ * @head: pointer to top element of the stack
+*/
+void pall(stack_t *head)
+{
+	stack_t *current = (stack_t *)head;
+
+	if (head == NULL)
+		return;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
